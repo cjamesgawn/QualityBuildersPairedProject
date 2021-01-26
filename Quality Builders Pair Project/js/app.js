b6d307111d0d6a68ac0666ed1f3334b50bd52f58
cjamesgawn <69963803+cjamesgawn@users.noreply.github.com>
2021-01-26 22:25:00 +0000

    update

---
+++ Quality Builders Pair Project/js/app.js
@@ -0,0 +1,12 @@
+$('document').ready(function(){
+
+    $('.slides').slick({
+        infinite: true,
+        autoplay: true,
+        autoplaySpeed: 5000,
+        arrows: true,
+        speed: 500,
+        
+    });
+
+});
